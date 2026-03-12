cb3e27af7b4f87a6e4bd967454547effe973084c
Saumyatripathi-12 <saumyatripathi723@gmail.com>
2026-03-12 19:48:29 +0530

    Time: 0 ms (100%), Space: 20.5 MB (40.13%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        from typing import List
+
+
+        num_map = {}   # dictionary to store number and its index
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            
+            if complement in num_map:
+                return [num_map[complement], i]
+            
+            num_map[num] = i
